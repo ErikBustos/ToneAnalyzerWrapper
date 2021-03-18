ea4c70664cb2adb390eb1c0901e72d8d7fc12fbf
erbustos <erbustos@ERBUSTOS-LAP.oradev.oraclecorp.com>
2021-03-18 12:48:15 -0600

    Comments added

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-// importamos las librerias importantes
+// Imports
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
 
-// importamos el router
-//const router = require('./src/routes')
-const ejercicios = require('./src/routes/index')
-// de express nos traemos lo necesario
 const { json, urlencoded } = express
 
-// creamos nuestra app
+
 const app = express()
 const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
-// definimos un puerto por el cual escucharemos peticiones
+// Port Config
 const PORT = process.env.PORT || 8080
 const HOST = process.env.HOST || "0.0.0.0"
 
-// configuraciones para nuestro server
+// server config
 app.use(json())
 app.use(urlencoded({ extended: false }))
 const corsOptions = { origin: '*', optionsSuccessStatus: 200 }
 app.use(cors(corsOptions))
 
+//toneAnalyzer setup.
 const toneAnalyzer = new ToneAnalyzerV3({
     version: '2017-09-21',
     authenticator: new IamAuthenticator({
@@ -32,10 +29,12 @@ const toneAnalyzer = new ToneAnalyzerV3({
     serviceUrl: `${process.env.APIURL}`,
   });
 
+//Base Get method
 app.get('/', (req, res) =>{
     res.send('Examen Cloud. Tone Analyzer Microservice');
 })
 
+//Get method to get the author details
 app.get('/autor', (req, res) => {
     res.send({
         alumno: 'EBM',
@@ -43,6 +42,7 @@ app.get('/autor', (req, res) => {
     })
 });
 
+//Post Method to use tone Analyzer API.
 app.post('/analyze', (req,res) => {
     let { input } = req.body;
     
@@ -63,8 +63,5 @@ app.post('/analyze', (req,res) => {
 
 })
 
-// indicamos que usaremos un router
-//app.use(router)
-//app.use('/',ejercicios)
 // iniciamos nuestro server
 app.listen(PORT,HOST, () => { console.log(`Server listening on port ${PORT} and host ${HOST}`); })
